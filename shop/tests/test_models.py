bb0919f359b1cf96a034f86bb79b2490233a6fe0
Anton <DarrenHD@yandex.ru>
2025-11-27 00:55:06 +0300

    Add unit tests for models and views

--- shop/tests/test_models.py
+++ shop/tests/test_models.py
@@ -1,38 +1,62 @@
 from django.test import TestCase
 from shop.models import Product, Purchase
-from datetime import datetime
+from datetime import date, datetime, timedelta
 
-class ProductTestCase(TestCase):
+class ProductModelTests(TestCase):
+    """Тесты для модели Product"""
+
     def setUp(self):
-        Product.objects.create(name="book", price="740")
-        Product.objects.create(name="pencil", price="50")
+        """Создание нескольких товаров для тестов"""
+        Product.objects.create(name="Книга", price=740)
+        Product.objects.create(name="Карандаш", price=50)
 
-    def test_correctness_types(self):                   
-        self.assertIsInstance(Product.objects.get(name="book").name, str)
-        self.assertIsInstance(Product.objects.get(name="book").price, int)
-        self.assertIsInstance(Product.objects.get(name="pencil").name, str)
-        self.assertIsInstance(Product.objects.get(name="pencil").price, int)        
+    def test_fields_types(self):
+        """Проверка типов полей name и price для созданных товаров"""
+        for product in Product.objects.all():
+            self.assertIsInstance(product.name, str)
+            self.assertIsInstance(product.price, int)
 
-    def test_correctness_data(self):
-        self.assertTrue(Product.objects.get(name="book").price == 740)
-        self.assertTrue(Product.objects.get(name="pencil").price == 50)
+    def test_price_values(self):
+        """Проверка корректности значений цены у товаров"""
+        book = Product.objects.get(name="Книга")
+        pencil = Product.objects.get(name="Карандаш")
+        self.assertEqual(book.price, 740)
+        self.assertEqual(pencil.price, 50)
 
 
-class PurchaseTestCase(TestCase):
+class PurchaseModelTests(TestCase):
+    """Тесты для модели Purchase"""
+
     def setUp(self):
-        self.product_book = Product.objects.create(name="book", price="740")
-        self.datetime = datetime.now()
-        Purchase.objects.create(product=self.product_book,
-                                person="Ivanov",
-                                address="Svetlaya St.")
+        """Создание продукта и покупки с сегодняшним днем рождения"""
+        self.product = Product.objects.create(name="Книга", price=740)
+        self.today = date.today()
+        self.yesterday = self.today - timedelta(days=1)
+        Purchase.objects.create(
+            product=self.product,
+            person="Ivanov",
+            address="Svetlaya St.",
+            birthday=self.today
+        )
 
-    def test_correctness_types(self):
-        self.assertIsInstance(Purchase.objects.get(product=self.product_book).person, str)
-        self.assertIsInstance(Purchase.objects.get(product=self.product_book).address, str)
-        self.assertIsInstance(Purchase.objects.get(product=self.product_book).date, datetime)
+    def test_purchase_field_types(self):
+        """Проверка типов полей person, address и date"""
+        purchase = Purchase.objects.get(product=self.product)
+        self.assertIsInstance(purchase.person, str)
+        self.assertIsInstance(purchase.address, str)
+        self.assertIsInstance(purchase.date, datetime)
 
-    def test_correctness_data(self):
-        self.assertTrue(Purchase.objects.get(product=self.product_book).person == "Ivanov")
-        self.assertTrue(Purchase.objects.get(product=self.product_book).address == "Svetlaya St.")
-        self.assertTrue(Purchase.objects.get(product=self.product_book).date.replace(microsecond=0) == \
-            self.datetime.replace(microsecond=0))
+    def test_date_auto_set(self):
+        """Проверка автоматического выставления даты создания покупки"""
+        purchase = Purchase.objects.get(product=self.product)
+        self.assertIsNotNone(purchase.date)
+
+    def test_purchase_creation_without_birthday_fails(self):
+        """Проверка, что покупка без обязательного поля birthday невозможна"""
+        with self.assertRaises(Exception):
+            Purchase.objects.create(
+                product=self.product,
+                person="Ivanov",
+                address="Svetlaya St."
+                # дня рождения нету тут
+            )
--- shop/tests/test_views.py
+++ shop/tests/test_views.py
@@ -1,10 +1,81 @@
 from django.test import TestCase, Client
-from shop.views import PurchaseCreate
+from shop.models import Product, Purchase
+from datetime import date
 
-class PurchaseCreateTestCase(TestCase):
+class IndexViewTests(TestCase):
+    """Тесты для функции представления index"""
+
     def setUp(self):
+        Product.objects.create(name="Что то", price=500)
         self.client = Client()
 
-    def test_webpage_accessibility(self):
+    def test_index_page_status_code(self):
+        """Проверка доступности главной страницы"""
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_index_page_context_contains_products(self):
+        """Проверка, что в контексте есть список продуктов"""
+        response = self.client.get('/')
+        self.assertIn('products', response.context)
+        self.assertTrue(len(response.context['products']) > 0)
+
+
+class PurchaseCreateViewTests(TestCase):
+    """Тесты для класса PurchaseCreate CreateView"""
+
+    def setUp(self):
+        self.client = Client()
+        self.product = Product.objects.create(name="Книга", price=740)
+        self.url = f'/buy/{self.product.id}/'
+
+    def test_get_purchase_form(self):
+        """GET-запрос должен отдавать статус 200 и передавать корректный контекст"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('product', response.context)
+        self.assertEqual(response.context['product'], self.product)
+        self.assertIn('price', response.context)
+        self.assertEqual(response.context['price'], self.product.price)
+
+    def test_post_creates_purchase_with_discount(self):
+        """POST с датой рождения сегодня должен получить скидку"""
+        response = self.client.post(self.url, data={
+            'person': 'Ivanov',
+            'birthday': date.today().isoformat(),
+            'address': 'Moscow',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Применена скидка')
+        self.assertContains(response, 'Итоговая цена')
+
+        purchase = Purchase.objects.latest('id')
+        self.assertEqual(purchase.person, 'Ivanov')
+        self.assertEqual(purchase.product, self.product)
+
+    def test_post_creates_purchase_without_discount(self):
+        """POST с датой рождения не сегодня не получает скидку("""
+        birthday = date(1990, 1, 1)
+        response = self.client.post(self.url, data={
+            'person': 'Petrov',
+            'birthday': birthday.isoformat(),
+            'address': 'Tver',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, 'Применена скидка')
+        self.assertContains(response, 'Итоговая цена')
+
+        purchase = Purchase.objects.latest('id')
+        self.assertEqual(purchase.birthday, birthday)
+
+    def test_post_missing_required_fields(self):
+        """POST без обязательных полей должен возвращать ошибку формы"""
+        response = self.client.post(self.url, data={
+            'person': '',
+            'birthday': '',
+            'address': '',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'form', 'person', 'This field is required.')
+        self.assertFormError(response, 'form', 'birthday', 'This field is required.')
+        self.assertFormError(response, 'form', 'address', 'This field is required.')
