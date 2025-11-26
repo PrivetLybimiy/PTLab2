354b8e345799e84910c65619ff0bf007b60ae3c8
Anton <DarrenHD@yandex.ru>
2025-11-27 00:26:51 +0300

    Fix tests

--- shop/tests/test_models.py
+++ shop/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from shop.models import Product, Purchase
-from datetime import datetime
+from datetime import datetime, date
 
 class ProductTestCase(TestCase):
     def setUp(self):
@@ -24,7 +24,8 @@ class PurchaseTestCase(TestCase):
         self.datetime = datetime.now()
         Purchase.objects.create(product=self.product_book,
                                 person="Ivanov",
-                                address="Svetlaya St.")
+                                address="Svetlaya St.",
+                                birthday=date.today())
 
     def test_correctness_types(self):
         self.assertIsInstance(Purchase.objects.get(product=self.product_book).person, str)
